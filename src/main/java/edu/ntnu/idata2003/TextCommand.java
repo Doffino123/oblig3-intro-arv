73f1c54393c3fd55fc572c696a8b05f1ce6f0014
Doffino <danjoh27@gmail.com>
2026-02-16 15:58:44 +0100

    created TextCommand class

---
+++ src/main/java/edu/ntnu/idata2003/TextCommand.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idata2003;
+
+/**
+ * Represents an action on a give string of text
+ * @version 2024.06.01
+ * @author Daniel Johansen
+ */
+public class TextCommand  {
+
+  private void String() {
+    execute(text);
+  }
+
+}
